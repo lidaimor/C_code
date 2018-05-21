1225f23236be0b1b4473d0ff7bc659b9cb39bfb5
lidaimor <lidaimor@163.com>
2018-05-21 19:59:08 +0800

    2

--- 奇数在前偶数在后.c
+++ 奇数在前偶数在后.c
@@ -1,21 +1,14 @@
 #include<stdio.h>
 int main(){
 	int a[8]={2,3,5,1,0,7,9,4};
-	int i,j,t1,t2;
-//	for(i=0;i<8;i++){
-//		if(a[i]%2){
-//			t=a[i];
-//			for(j=i;j>=0;j--){
-//				a[j]=a[j-1];
-//			}
-//			a[0]=t;
-//		}
-//	}
-	i=0;j=7;
-	while(i<j){
-		while(a[i]%2){
-			t1=a[i];
-			i++;
+	int i,j,t;
+	for(i=0;i<8;i++){
+		if(a[i]%2){
+			t=a[i];
+			for(j=i;j>=0;j--){
+				a[j]=a[j-1];
+			}
+			a[0]=t;
 		}
 	}
 	for(i=0;i<8;i++){
